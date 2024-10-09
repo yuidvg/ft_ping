75f545eeddc6d768db88a1baaf281520ecaa43d8
Yui Nishimura <nisshi.yui79@gmail.com>
2024-10-09 09:34:19 +0200

    move rowSock creation out of the loop

--- src/main.c
+++ src/main.c
@@ -2,35 +2,37 @@
 
 int main(int ac, char *av[])
 {
-	if (ac == 1)
-	{
-		printf("ping: missing host operand\nTry 'ping --help' or 'ping --usage' for more information.\n");
-	}
-	else if (ac == 2)
-	{
-		int sequenceNumber = 0;
-		while (1)
-		{
-			// Create raw socket
-			int rawSockfd = createRawSocketOrExitFailure();
-			
-			//Send ICMP Echo Request
-			const IcmpEchoHeader icmpEchoHeader = constructIcmpEchoHeader(getpid(), sequenceNumber);
+    if (ac == 1)
+    {
+        printf("ping: missing host operand\nTry 'ping --help' or 'ping --usage' for more information.\n");
+    }
+    else if (ac == 2)
+    {
+        // Create raw socket
+        int rawSockfd = createRawSocketOrExitFailure();
 
-			// Destination address
-			struct sockaddr_in dest_addr;
-			memset(&dest_addr, 0, sizeof(dest_addr));
-			dest_addr.sin_family = AF_INET;
-			dest_addr.sin_addr.s_addr = inet_addr(av[1]);
+        int sequenceNumber = 0;
+        while (1)
+        {
+            // Send ICMP Echo Request
+            const IcmpEchoHeader icmpEchoHeader = constructIcmpEchoHeader(getpid(), sequenceNumber);
 
-			// Send ICMP packet
-			if (sendto(rawSockfd, &icmpEchoHeader, sizeof(icmpEchoHeader), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr)) <= 0) {
-				perror("sendto");
-				close(rawSockfd);
-				exit(EXIT_FAILURE);
-			}
-			//Recive ICMP Echo Reply and Print (Maybe Somewhere Else Like in A Handler)
-			++sequenceNumber;
-		}
-	}
+            // Destination address
+            struct sockaddr_in dest_addr;
+            memset(&dest_addr, 0, sizeof(dest_addr));
+            dest_addr.sin_family = AF_INET;
+            dest_addr.sin_addr.s_addr = inet_addr(av[1]);
+
+            // Send ICMP packet
+            if (sendto(rawSockfd, &icmpEchoHeader, sizeof(icmpEchoHeader), 0, (struct sockaddr *)&dest_addr,
+                       sizeof(dest_addr)) <= 0)
+            {
+                perror("sendto");
+                close(rawSockfd);
+                exit(EXIT_FAILURE);
+            }
+            // Recive ICMP Echo Reply and Print (Maybe Somewhere Else Like in A Handler)
+            ++sequenceNumber;
+        }
+    }
 }
